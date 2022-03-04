0e4087fec19b6271becead844cc6acb0cc000cd3
John Titor <johntitor@future.com>
2022-03-04 10:36:37 -0300

    Bumping version

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ export const PORT = 443;
 export const ZONE = "BomberGameZone";
 export const SALT_KEY = "f17e4e44f7bbc229fdc2d2f55728abba";
 export const DATE_OFFSET = 62135596800000;
-export const VERSION_CODE = 33;
+export const VERSION_CODE = 34;
 export const LT = 0;
 export const LC = "";
