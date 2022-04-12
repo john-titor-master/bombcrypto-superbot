637a9f69ea6a60ceb016e4f6a52db770fb11db32
lucas vieceli <vieceli46@hotmail.com>
2022-04-12 11:00:13 -0300

    update logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,15 @@
 import winston from "winston";
-import { askAndParseEnv, parseBoolean } from "./lib";
+import { askAndParseEnv, parseBoolean, requireEnv } from "./lib";
 
 const formatter = winston.format.printf(({ level, message, timestamp }) => {
     return `${timestamp} [${level}]: ${message}`;
 });
 
+
+const level = requireEnv("DEBUG_LEVEL") || "info";
+
 export const logger = winston.createLogger({
-    level: askAndParseEnv("DEBUG", parseBoolean, false) ? "debug" : "warning",
+    level: askAndParseEnv("DEBUG", parseBoolean, false) ? "debug" : level,
     format: winston.format.combine(winston.format.timestamp(), formatter),
     transports: [new winston.transports.Console()],
 });
