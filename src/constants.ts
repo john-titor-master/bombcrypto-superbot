e427ec6d337649b6918dadb548da71b19cdafed5
lucas vieceli <vieceli46@hotmail.com>
2022-06-24 08:18:26 -0300

    update version

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const PORT = 443;
 export const ZONE = "BomberGameZone";
 export const SALT_KEY = "f17e4e44f7bbc229fdc2d2f55728abba";
 export const DATE_OFFSET = 62135596800000;
-export const VERSION_CODE = 54;
+export const VERSION_CODE = 55;
 export const LC = "";
