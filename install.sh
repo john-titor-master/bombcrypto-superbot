c74700054675bb153f8100c5d041abdd45b70e9e
teste <teste@teste.com>
2023-03-09 14:49:18 -0300

    .

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 sudo apt update
 apt-get install unzip wget -y
 
-if [ "$(uname -m)" == "arm64" ]; then
+if [ "$(uname -m)" == "aarch64" ]; then
     # Se for ARM64
-    echo "not supported arm64"
+    URL="https://github.com/lucasvieceli/bombcrypto-superbot/releases/latest/download/bot-arm64.zip"
     exit 1
-else
+else if [ "$(uname -m)" == "x86_64" ]; then
     # Se for x64
     URL="https://github.com/lucasvieceli/bombcrypto-superbot/releases/latest/download/bot-x64.zip"
+else
+    echo "not supported"
+     exit 1
 fi
 
 
